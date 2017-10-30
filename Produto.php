d51fe183a25a982cc3c05f178f9892116faf18e2
formiga1995 <gmfformiga@gmail.com>
2017-10-30 09:54:31 -0200

    Update 1.1

--- Produto.php
+++ Produto.php
@@ -2,200 +2,12 @@
 <head>
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Lato|Roboto" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="estiloproduto.css">
     <title>Pub</title>
-    <style>
-            body{
-                background-color: #fbfbfa;
-            }
-            .menu{
-                margin: auto;
-                margin-top: 50px;
-                padding: 40px;
-                width: 500px;
-                height: 540px;
-                border-radius: 4px;
-                background-color: #fdfdfd;
-                box-shadow: 0px 2px 4px 0px #999;
-                text-align: center;
-            }
-            .search{
-                margin: auto;
-                width: 500px;
-                height: 44px;
-                padding: 4px;
-                border: 1px;
-                border-radius: 4px;
-                background-color: #fefefe;
-                box-shadow: 0px 1px 2px 0px #999;
-                font-family: "Montserrat", Times, serif;
-            }
-            .search:hover{
-                box-shadow: 0px 1px 3px 0px #999;
-            }
-            .entre{
-                float: left;
-                margin: auto;
-                width: 480px;
-                height: 100%;
-                padding: 16px;
-                border: 0px;
-                outline: 0px;
-                background-color: transparent;
-                color: #666;
-				font-size: 18px;
-                font-family: "Montserrat", Times, serif;
-            }
-			.linha{
-                margin-top: 40px;
-                margin-left: -40px;
-                width: 580px;
-                height: 1px;
-                background-color: #aeaeae;
-			}
-			table, th, td {
-				width: 500px;
-                padding: 8px;
-				border: 0px;
-				border-collapse: collapse;
-				color: #666;
-                text-align: left;
-				font-size: 14px;
-                font-family: "Roboto", serif;
-				font-weight: 100;
-                transition: 1s;
-			}
-			table{
-				margin-top: 40px;
-                box-shadow: 0px 1px 3px 0px #aaa;
-			}
-			th{
-				height: 36px;
-				border: 0px solid #119911;
-				background-color: #33bb55;
-				color: #fefefe;
-				font-size: 18px;
-				text-align: center;
-				font-style: thin;
-                letter-spacing: 1px;
-			}
-			td{
-				border: 0px;
-				border-bottom: 1px solid #ddd;
-			    text-align: center;
-                letter-spacing: 1px;
-			}
-            tr{
-                transition: .3s;
-            }
-            tr:not(:first-child):hover{
-                box-shadow: 0px 1px 10px 0px #aaa;
-            }
-            td:hover{
-                text-shadow: 0px 1px #ccc;
-            }
-            .entab{
-                margin: auto;
-                width: 100%;
-                height: 50%;
-                padding: 4px;
-                padding-top: 14px;
-                padding-bottom: 14px;
-                border: 0px;
-                outline: 0px;
-                background-color: transparent;
-				text-align: center;
-                color: #eee;
-				font-size: 18px;
-                font-family: "Montserrat", Times, serif;
-            }
-            .entab:focus{
-                border-bottom: 2px solid #eee;
-            }
-            .item{
-                margin: auto;
-                margin-top: 40px;
-                padding: 4px;
-                width: 320px;
-                height: 44px;
-                border-radius: 4px;
-                border: 0px;
-                background-color: #33bb55;
-                font-family: "Montserrat", Times, serif;
-                color: #fefefe;
-                outline:0;
-                
-                font-size: 18px;
-                letter-spacing: 1px;
-                transition: .3s;
-                /*box-shadow: inset 0 0 0 5em rgba(255,255,255,0.2);*/
-                box-shadow: 0px 1px 2px 0px #999;
-                transition: box-shadow 0.8s;
-                list-style-type:none;
-                display:inline-block;
-                line-height:2em;
-                color:#fff;
-                position:relative;
-                overflow:hidden;
-                transition: 0.2s;
-            }
-            button {
-                position: relative;
-                overflow: hidden;
-                padding: 16px 32px;
-            }
-            button:after {
-                content: '';
-                display: block;
-                position: absolute;
-                left: 50%;
-                top: 50%;
-                width: 420px;
-                height: 420px;
-                margin-left: -210px;
-                margin-top: -210px;
-                /*background: #3f51b5;*/
-                background: #fff;
-                border-radius: 100%;
-                opacity: .2;
-                transform: scale(0);
-            }
-            @keyframes ripple {
-                0% {
-                    transform: scale(0);
-                }
-                20% {
-                    transform: scale(1);
-                }
-                100% {
-                    opacity: 0;
-                    transform: scale(1);
-                }
-            }
-            button:not(:active):after {
-                animation: ripple 2.5s ease-out;
-            }
-            /* fixes initial animation run, without user input, on page load.
-            */
-            button:after {
-                visibility: hidden;
-            }
-            button:focus:after {
-                visibility: visible;
-            }
-            .item:hover, .botao:focus{
-                background-color: #33bf45;
-                box-shadow: 0px 1px 2px 0px #999;
-                /*border-radius: 50%;*/
-            }
-            ::-webkit-input-placeholder, select.first-child { /* WebKit, Blink, Edge */
-                color: #aaa;
-            }
-            .entab::-webkit-input-placeholder { /* WebKit, Blink, Edge */
-                color: #fff;
-                opacity: 0.7;
-            }
-            
-    </style>
+    
 </head>
 <body>
 
@@ -237,6 +49,9 @@
         ?>
 		</table>
         <button class="item">Adicionar Produto</button><br>
+        <button class="item" onclick="window.location.href='\index.html'">Voltar</button>
+
     </div>
-</body>
+            
+    </body>
 </html>
