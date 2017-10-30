83bae58ad7b589235daf2e7a4f6656442ab32dd1
Gabriel <gmfformiga@gmail.com>
2017-10-30 15:18:09 -0200

    Nova Pagina de Reserva (inacabada)

--- reservarmesa.php
+++ reservarmesa.php
@@ -6,32 +6,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="css\estiloproduto.css">
-    <title>Reservas</title>
+    <title>Reservar Mesa</title>
     
 </head>
 <body>
 
     <div class="menu">
         <div   class="search">
-            <input class="entre" placeholder="Pesquise aqui..."></input>
+            <input class="entre" placeholder="ID_CLIENTE"></input>
         </div>
-        <div class="linha" align='center'></div>
-        <table class="tabela">
-            <tr>
-                <th><input class="entab" placeholder="MESA"></input></th>
-                <th><input class="entab" placeholder="CLIENTE"></input></th> 
-                <th><input class="entab" placeholder="HORARIO"></input></th>
-            </tr>
+            <div   class="search">
+            <input class="entre" placeholder="ID_MESA"></input>
+        </div>
+        <div   class="search">
+            <input class="entre" placeholder="DATA_HORA_RESERVA"></input>
+        </div>
+        
+        
             
 <?php  //Isso aqui tá só pra dar o select, mas tem que alterar o html pra mostrar de acordo com o resultado da consulta
                     include 'conectar.php';
                 //$database = new $conn;
                 //$db = ;
-                $consulta_reserva     =   "SELECT A.ID_CLIENTE,A.ID_MESA,A.DATA_HORA_RESERVA,B.ID_CLIENTE,B.NOME 
+                $insere_reserva     =   "INSERT A.ID_CLIENTE,A.ID_MESA,A.DATA_HORA_RESERVA,B.ID_CLIENTE,B.NOME 
                                             FROM RESERVA A, CLIENTE B
                                             WHERE A.ID_CLIENTE = B.ID_CLIENTE;";
                 //$resultado_consulta = mysqli_query($conn, $consulta_produto);
-               $result = $conn->query($consulta_reserva);
+               $result = $conn->query($insere_reserva);
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
@@ -47,7 +48,7 @@
         }
         ?>
         </table>
-        <button class="item" onclick="window.location.href='\rreservarmesa.php'">Reservar mesa</button><br>
+        <button class="item" onclick="window.location.href='\.html'">Reservar mesa</button><br>
         <button class="item" onclick="window.location.href='\index.html'">Voltar</button>
 
     </div>
