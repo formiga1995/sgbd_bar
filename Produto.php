bacae9c0cdbdc8a6d1850b239a46368ed9218d9a
arturmntr <artur.mntr@gmail.com>
2017-10-30 22:35:21 -0200

    Update - Tela para Adicionar Produto

--- Produto.php
+++ Produto.php
@@ -5,7 +5,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="estiloproduto.css">
+    <link rel="stylesheet" type="text/css" href=""css\estiloproduto.css">
     <title>Pub</title>
     <style>
         
